0de9848ce7366410c831d374a1308d582eca54ff
HagosHaile <hagosb.haile@gmail.com>
2019-04-03 21:10:05 -0500

    iterator pattern

---
+++ iterator/src/com/hagos/iterator/Iterator.java
@@ -0,0 +1,9 @@
+package com.hagos.iterator;
+
+public interface Iterator<T> {
+	
+	boolean hasNext();
+	
+	T next();
+
+}
---
+++ iterator/src/com/hagos/iterator/ThemeColor.java
@@ -0,0 +1,30 @@
+package com.hagos.iterator;
+
+public enum ThemeColor {
+	
+	RED,
+	ORANGE,
+	BLACK,
+	WHITE;
+	
+	public static Iterator<ThemeColor> getIterator(){
+		return new ThemeColorIterator();
+	}
+	
+	private static class ThemeColorIterator implements Iterator<ThemeColor>{
+
+		private int pos;
+		@Override
+		public boolean hasNext() {
+			return pos < ThemeColor.values().length;
+		}
+
+		@Override
+		public ThemeColor next() {
+			
+			return ThemeColor.values()[pos++];
+		}
+		
+	}
+
+}
